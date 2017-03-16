ac006e73a5545d04c04da5235002f380a677bb52
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-03-16 16:27:25 -0400

    have test pass for converting string into float types ready for calculations

--- lib/nupack/markup.rb
+++ lib/nupack/markup.rb
@@ -1,4 +1,7 @@
 module Nupack
   class Markup
+    def self.total(base_price,workers,type)
+      new_price = base_price.delete("$,").to_f
+    end
   end
 end
--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Nupack::Markup do
   describe ".total" do
     # convert "$100,000.00" to float for calculation
-    context "given '$100.00','1 person','food'" do
+    describe "given '$100.00'" do
       it "should return '100.0'" do
         expect(Nupack::Markup.total('$100.00','1 person','food')).to eql(100.0)
       end
