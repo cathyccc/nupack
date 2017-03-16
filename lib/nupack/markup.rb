9c601ca71ed023721f7935f07e5238d4a314ba9f
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-03-16 16:49:25 -0400

    have test for 5% markup on base price pass

--- lib/nupack/markup.rb
+++ lib/nupack/markup.rb
@@ -2,6 +2,7 @@ module Nupack
   class Markup
     def self.total(base_price,workers,type)
       new_price = base_price.delete("$,").to_f
+      new_price*1.05
     end
   end
 end
--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -3,9 +3,16 @@ require "spec_helper"
 describe Nupack::Markup do
   describe ".total" do
     # convert "$100,000.00" to float for calculation
-    describe "given '$100.00'" do
-      it "should return '100.0'" do
-        expect(Nupack::Markup.total('$100.00','1 person','food')).to eql(100.0)
+    # context "given '$100.00'" do
+    #   it "should return '100.0'" do
+    #     expect(Nupack::Markup.total('$100.00','1 person','food')).to eql(100.0)
+    #   end
+    # end
+
+    # markup flat rate by 5%
+    context "given flat rate of '$10,000.00'" do
+      it "should return '10500.00'" do
+        expect(Nupack::Markup.total("$10,000.00","1 person","food")).to eql(10500.0)
       end
     end
   end
