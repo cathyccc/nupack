fd66738d450a78faa61adc34bc28dc31a35892de
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-03-16 17:47:01 -0400

    have new test for number of workers pass with additional markup

--- lib/nupack/markup.rb
+++ lib/nupack/markup.rb
@@ -1,8 +1,16 @@
 module Nupack
   class Markup
     def self.total(base_price,workers,type)
+      total = 0
       new_price = base_price.delete("$,").to_f
-      (new_price*1.05).round(2)
+      new_markup = (new_price*1.05).round(2)
+      total = new_markup
+
+      # regular expression to grab first character that matches "1-9" followed by infinite
+      # characters(*) from "0-9"
+      regex=/[1-9][0-9]*/
+      num_workers = workers.scan(regex)[0].to_i
+      total += (new_markup * (0.012 * num_workers)).round(2)
     end
   end
 end
--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -9,16 +9,17 @@ describe Nupack::Markup do
     #   end
     # end
 
-    # markup flat rate by 5%
-    context "given flat rate of '$10,000.00'" do
-      it "should return '10500.00'" do
-        expect(Nupack::Markup.total("$10,000.00","1 person","food")).to eql(10500.0)
+    # new flat markup = base price times 5%
+    # for each person that needs to work on the job, there is a markup of 1.2%
+    context "given flat rate of '$10,000.00' and '1 person'" do
+      it "should return 1.2% markup on new flat markup of 10500.00" do
+        expect(Nupack::Markup.total("$10,000.00","1 person","food")).to eql(10626.0)
       end
     end
 
-    context "given flat rate of '$1,299.99'" do
-      it "should return '1364.99'" do
-        expect(Nupack::Markup.total("$1,299.99","1 person","food")).to eql(1364.99)
+    context "given flat rate of '$1,299.99' and '80 aliens'" do
+      it "should return 96% markup on new flat markup of 1364.99" do
+        expect(Nupack::Markup.total("$1,299.99","80 aliens","food")).to eql(2675.38)
       end
     end
 
